5a78d126f1b7ec03b132be4566a4119b51e502e7
Rafael Marcos <rafix995@gmail.com>
2025-07-19 05:03:39 +0100

    Placing existing elements in the main screen

--- app/src/main/java/dev/rmarcos/currencyxchange/MainActivity.kt
+++ app/src/main/java/dev/rmarcos/currencyxchange/MainActivity.kt
@@ -4,14 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.google.android.gms.ads.MobileAds
+import dev.rmarcos.currencyxchange.mainscreen.AdBanner
+import dev.rmarcos.currencyxchange.mainscreen.ConvertedValue
 import dev.rmarcos.currencyxchange.ui.theme.CurrencyXchangeTheme
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -42,10 +48,29 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainScreen(modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello!",
+    Column(
         modifier = modifier
-    )
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        AdBanner(
+            bannerId = stringResource(R.string.TOP_AD_BAR)
+        )
+        Spacer(
+            Modifier.weight(1f)
+        )
+        ConvertedValue(
+            value = 0f
+        ) {
+            // TODO
+        }
+        Spacer(
+            Modifier.weight(1f)
+        )
+        AdBanner(
+            bannerId = stringResource(R.string.TOP_AD_BAR)
+        )
+    }
 }
 
 @Preview(showBackground = true)
