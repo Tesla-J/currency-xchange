edb5319e01f902ffa48f4c47f06e4a73b0feea9b
Rafael Marcos <rafix995@gmail.com>
2025-07-28 03:04:12 +0100

    include retrofit dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,6 +67,10 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation("com.google.android.gms:play-services-ads:24.4.0")
+    // Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+// Retrofit with Scalar Converter
+    implementation("com.squareup.retrofit2:converter-scalars:2.9.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
